a146b65d1e318fa3137e84abbefa002ead5cbcf0
test.com <test@test.com>
2020-05-28 06:44:57 +0800

    第一次提交至本地仓库,但是位提交到远程仓库;

--- src/main/java/com/gcnzjc/helloworld/HelloWorld.java
+++ src/main/java/com/gcnzjc/helloworld/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("Hello world");
         sum(10,20);
+        System.out.println("第一次提交本地仓库,未提交至远程仓库;");
 
 
     }
