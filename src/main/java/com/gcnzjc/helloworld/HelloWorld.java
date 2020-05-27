8c43ad6acfb35427243e1aa77f89a14e82899b92
test.com <test@test.com>
2020-05-28 07:22:15 +0800

    new branch

--- src/main/java/com/gcnzjc/helloworld/HelloWorld.java
+++ src/main/java/com/gcnzjc/helloworld/HelloWorld.java
@@ -7,7 +7,9 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("Hello world");
         sum(10,20);
-        System.out.println("new branch");
+        System.out.println("第一次提交本地仓库,未提交至远程仓库;");
+        System.out.println("既提交到本地仓库,又提交到远程仓库,查看远程仓库提交记录是否显示上一次记录");
+
 
     }
 
