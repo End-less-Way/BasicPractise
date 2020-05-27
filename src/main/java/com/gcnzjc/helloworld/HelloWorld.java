835e607bf779608325308e4ae10ceb6c90d8c819
test.com <test@test.com>
2020-05-27 19:40:40 +0800

    ver02 hello world

--- src/main/java/com/gcnzjc/helloworld/HelloWorld.java
+++ src/main/java/com/gcnzjc/helloworld/HelloWorld.java
@@ -3,6 +3,9 @@ package com.gcnzjc.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
 
